33ae40849d9b1ef484fb77fac6d3d5a72608cd80
Natdenice <Clad1987@hotmail.com>
2019-01-18 17:08:53 +0100

    added a class toggle when key pressed and sync keys with sounds completed

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 window.addEventListener('keydown', function(e) {
   var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return;
+  audio.currentTime = 0;
   audio.play();
+  console.log(key);
+  key.classList.toggle('playing');
+
 });
 
 
