c1fe1f848a20dedaabf2048f0219cc81b83b38e8
Natdenice <Clad1987@hotmail.com>
2019-01-18 16:57:22 +0100

    key events listeners synchronized to play sounds

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-var x = document.getElementsByClassName("key"); 
+window.addEventListener('keydown', function(e) {
+  var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.play();
+});
+
+
+
+/*var x = document.getElementsByClassName("key"); 
 
 for (var i = 0; i < key.length; i++) {
   key[i].addEventListener('click', function() {
   key.classList.add('playing');
 })
-};
+};*/
 
 
 
